28cb9529796cd3d46ffb122fcf6cc465f6b337aa
Bar <barshenig@gmail.com>
2026-04-19 19:59:53 +0300

    Commit the main terraform file to the repo

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+     }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "myRSVP"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+
+
+  tags = {
+    Owner = "RSVP"
+    Environment = "dev"
+  }
+}
