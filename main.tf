b35fa0ecf87e381cb977cf89fb6a52238894a5cf
Bar <barshenig@gmail.com>
2026-04-26 20:39:07 +0300

    commit the main terraform that includes the security group rules

--- main.tf
+++ main.tf
@@ -22,6 +22,24 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 
+  manage_default_security_group = true
+  default_security_group_ingress = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "Allow HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  default_security_group_egress = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
 
   tags = {
     Owner = "RSVP"
