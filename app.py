e330a05e020e34c80178d699985bb0b493813853
Bar <barshenig@gmail.com>
2026-06-14 18:16:23 +0300

    Adding a line command to test the workflow

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from authlib.integrations.flask_client import OAuth
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Attr
 
+### adding a line for testing the workflow
 
 app = Flask(__name__)
 
